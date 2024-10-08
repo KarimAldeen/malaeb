7d7b50fcef0b8a7ff914a1e7a9e5515eff59e50c
karimaldeen <mhmadkarimaldeen@gmail.com>
2024-10-09 00:19:10 +0300

    first

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import logo from './assets/image.png'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>
+        PLAY. CONNECT. COMPETE.
+      </h1>
+      <div>
+        <div className='divAr'>
+         
+      
+        قـــــــــــادم
+        <br/>
+قـــريبـاً
+
+
+        </div>
+        <img src={logo} alt="" />
+        <div>
+       
+
+        COMING
+        <br/>
+          SOON
+
+        </div>
+      </div>
+      <h2>
+      الــــــــــــعـــــــــــب. تـــــــــــــواصــــــــــــــــل. نـــــــــــــــافـــــــــــــس.
+
+
+      </h2>
+
+    </div>
+  )
+}
+
+export default App
